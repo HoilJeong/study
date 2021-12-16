43e0cd97a386754be8bfd0f729655fc95b7ebc07
HoilJeong <93916454+HoilJeong@users.noreply.github.com>
2021-12-16 20:04:15 +0900

    Add files via upload

---
+++ 03_floatint_point/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace _03_floatint_point
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // float 형식 변수에 값을 직접 할당하려면 숫자 뒤에 f를 붙여줘야 함
+            float a = 3.1415_9265_3589_7932_3846f;
+            Console.WriteLine(a);
+
+            double b = 3.1415_9265_3589_7932_3846;
+            Console.WriteLine(b);
+        }
+            
+    }
+}
---
+++ 05_object/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _05_object
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 모든 데이터 형식은 object 를 상속 받는다.
+            // 어떤 형식의 데이터라도 object에 담아 처리할 수 있다.
+            object a = 123;
+            object b = 3.14159265358979323846m;
+            object c = true;
+            object d = "안녕하세요.";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+            Console.WriteLine(c);
+            Console.WriteLine(d);
+        }
+    }
+}
